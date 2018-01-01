b326a968d6fb64eef7bec5e4fe40d3a89c4359ec
pooja jain <poojajain9450@gmail.com>
2018-01-01 19:50:47 +0530

    2ndpass

--- components/gallery/galleryController.js
+++ components/gallery/galleryController.js
@@ -1,9 +1,19 @@
-rootModule.controller('galleryController', ['$scope', 'galleryService', function($scope, galleryService) {
-    $scope.gallery = [];
+rootModule.controller('galleryController', ['$scope', 'galleryService', '$timeout', function($scope, galleryService, $timeout) {
+     $scope.gallery = [];
     galleryService.getGallery().then(function(response) {
         console.log(response);
         $scope.gallery = response.data;
     }, function() {
         console.log('Error during gallery data fetching!');
+    });
+
+    $timeout(function() {
+        $('.thumbnail').click(function(){
+            $('.modal-body').empty();
+          var title = $(this).parent('a').attr("title");
+          $('.modal-title').html(title);
+          $($(this).parents('div').html()).appendTo('.modal-body');
+          $('#myModal').modal({show:true});
+      });
     });
 }]);

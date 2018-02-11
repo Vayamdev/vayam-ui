eb10ebed87f9ac0b5129a1c43f6d346b7b4e102e
sayaliga <31365282+sayaliga@users.noreply.github.com>
2018-02-11 18:48:58 +0530

    Update headerDirective.js

--- shared/header/headerDirective.js
+++ shared/header/headerDirective.js
@@ -2,7 +2,7 @@ rootModule.directive('vayamHeader', function(){
     return {
         restrict: 'A',
         templateUrl: '/shared/header/headerTemplate.html',
-        scope: false,
+        scope: true,
         controller: ['$scope', '$route', function($scope, $route) {
             $scope.$on("$routeChangeSuccess", function(event, next, current) {
                 $scope.selected = $route.current.activetab;

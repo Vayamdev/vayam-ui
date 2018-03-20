1e8e55c18082a5aa2ad78f089cfe49203a4223ae
Sanket Joshi <sanketjoshi96@gmail.com>
2018-03-20 23:30:40 +0530

    Adding data for teams tab

--- components/aboutus/team/teamController.js
+++ components/aboutus/team/teamController.js
@@ -1,5 +1,5 @@
 rootModule.controller('teamController', ['$scope', 'teamService', 'globalFactory', function($scope, teamService, globalFactory) {    
-    $scope.teamgroup = {};
+    $scope.teamdata = [];
     $scope.bannerUrl = 'http://placehold.it/1146x400';
     $scope.bannertext = '';
 
@@ -9,12 +9,8 @@ rootModule.controller('teamController', ['$scope', 'teamService', 'globalFactory
     });    
     
     teamService.getStaff().then(function(response) {
-            var teamdata = response.data;
-            var key = 0;
-            while(teamdata.length > 0) {
-                ++key;
-                $scope.teamgroup[key.toString()] = teamdata.splice(0, 4);
-            }
+        console.log(response.data);
+        $scope.teamdata = response.data;
         }, function() {
             console.log('Error during team data fetching!');
     });
